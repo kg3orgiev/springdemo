0dc94ce59dcd449d0b25742de7090fe74b80ca70
Kosta Georgiev <kosta.georgiev@dreamix.eu>
2023-07-14 15:31:56 +0300

    4

--- mycoolapp/src/main/java/com/k0c3/spring/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/k0c3/spring/demo/mycoolapp/rest/FunRestController.java
@@ -1,2 +1,29 @@
-package com.k0c3.spring.demo.mycoolapp.rest;public class FunRestController {
+package com.k0c3.spring.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World 2";
+    }
+
+    @GetMapping("/teaminfo")
+    public String sayTeamInfo(){
+        return "Team : " + teamName + " has coach " + coachName;
+    }
+
+    @GetMapping("/fortune")
+    public String sayFortune(){
+        return "Today is your lucky day";
+    }
 }
